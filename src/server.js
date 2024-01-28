34afaef577bfc8438a948bf942bd5dbf5fe0f25c
PapiLuee <41023873+LueDev@users.noreply.github.com>
2024-01-27 23:27:12 -0500

    Added json server things

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-const jsonServer = require("json-server")
-const server = jsonServer.create()
-const router = jsonServer.router("../db.json")
-const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 4000
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(
+  "/Users/luisjorge/code/Flatiron-Phase-2/16-Phase-2-Project/titan-capital-financial/db.json"
+);
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4000;
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 
-server.listen(port)
+server.listen(port);
